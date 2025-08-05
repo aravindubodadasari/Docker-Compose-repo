553afc101b6bbbfed75e92c26c4e531e1e84b879
aravindu <aravindb402@gmail.com>
2025-08-05 15:26:51 +0530

    changes done

--- Docker-compose with anglar and spring microservice with eureka server/frontend/frontend-app/src/app/login.service.ts
+++ Docker-compose with anglar and spring microservice with eureka server/frontend/frontend-app/src/app/login.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class LoginService {
  //baseUrl:string ="http://localhost:9090/login";
-baseUrl:string ="http://18.212.164.128:9090/login";
+baseUrl:string ="http://3.110.162.184:9090/login";
   constructor(public httpClient:HttpClient) { }
 
   signIn(login:any):Observable<string> {
